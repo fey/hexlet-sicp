927cad1379068b68e66589b9227b142c30488f51
feycot <feycot@gmail.com>
2022-12-20 00:33:25 +0500

    --wip-- [skip ci]

---
+++ database/migrations/2022_12_11_124515_add_state_column_to_read_chapters.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\ReadChapter;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('read_chapters', function (Blueprint $table) {
+            $table->string('state')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('read_chapters', function (Blueprint $table) {
+            $table->dropColumn('state');
+        });
+    }
+};
