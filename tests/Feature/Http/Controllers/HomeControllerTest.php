e851609fc332a0b7a2f66055944ac4bd67f04270
feycot <feycot@gmail.com>
2023-06-05 22:51:49 +0500

    add title attributes

--- tests/Feature/Http/Controllers/HomeControllerTest.php
+++ tests/Feature/Http/Controllers/HomeControllerTest.php
@@ -3,9 +3,12 @@
 namespace Tests\Feature\Http\Controllers;
 
 use Tests\TestCase;
+use Qameta\Allure\Attribute\Title;
 
+#[Title("Проверка главной страницы")]
 class HomeControllerTest extends TestCase
 {
+    #[Title("Возможность открытия главной страницы гостем")]
     public function testIndex(): void
     {
         $response = $this->get(route('home'));
@@ -13,6 +16,7 @@ class HomeControllerTest extends TestCase
         $response->assertOk();
     }
 
+    #[Title("Ссылка для dev-логина не видна")]
     public function testNotSeeDevLogin(): void
     {
         $response = $this->get(route('home'));
